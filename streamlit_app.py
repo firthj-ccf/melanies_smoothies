3bcb0f06f3cc56289de1adb808ed569b2c487d8d
firthj-ccf <firthj@ccf.org>
2024-12-12 17:01:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert snowpark to Pandas
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_pf)
+st.dataframe(pd_df)
 st.stop()
 
 
